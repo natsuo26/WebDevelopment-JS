4abbcc6fe2a3682ca3863de1ed64a87641977d88
Abhay Kumar <abhaydkumar80@gmail.com>
2022-04-10 14:10:47 +0530

    Update script.js
    changes for 10 apr 21

--- complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -1,5 +1,25 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
+//npm liveserver -g
+//live-server
+// const xerolaz = 23;
+// console.log(xerolaz);
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
-const xerolaz = 23;
-console.log();
+const calcTempAmplitude=function(temps){
+    // let minTemp=Math.min(temps);
+    
+    let max=temps[0];
+    let min=temps[0];
+    for(let i=0;i<temps.length;i++)
+    {
+        let curTemp=temps[i];
+        if(curTemp>max) max=curTemp;
+        if(curTemp<min) min=curTemp;
+    }
+    console.log(max);
+    console.log(min);
+    return max -min;
+};
+
+console.log(`the amplitude is: ${calcTempAmplitude(temperatures)}`);
